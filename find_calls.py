36944f7b192c7f7fe4354a727c9db39fb6bdef51
Jon Poindexter <7112421+W5ALC@users.noreply.github.com>
2024-03-29 23:08:19 -0600

    Update find_calls.py
    Added additional formatting to display available calls in sorted by call district and alphabetically.

--- find_calls.py
+++ find_calls.py
@@ -6,19 +6,19 @@ import csv
 
 def chars(first, last):
     res = []
-    for c in range(ord(first),ord(last)+1):
-        res.append( chr(c) )
+    for c in range(ord(first), ord(last)+1):
+        res.append(chr(c))
     return res
     
 def alphabet():
-    return chars('A','Z')
+    return chars('A', 'Z')
 
 def numbers():
-    return chars('0','9')
+    return chars('0', '9')
 
 def two_by_one(remove_11_13=True):
     res = []
-    for a in ['A','K','N','W']:
+    for a in ['A', 'K', 'N', 'W']:
         for b in alphabet():
             for c in numbers():
                 for d in alphabet():
@@ -35,22 +35,44 @@ def two_by_one(remove_11_13=True):
                         # Two letter prefixes that are designated for regions
                         # 11-13 are not available in regions 1-10.
                         continue
-                    res.append( a+b+c+d )
+                    res.append(a+b+c+d)
     return res
 
 def one_by_two():
     res = []
-    for a in ['K','N','W']:
+    for a in ['K', 'N', 'W']:
         for b in numbers():
             for c in alphabet():
                 for d in alphabet():
-                    #print a+b+c+d
-                    res.append( a+b+c+d )
+                    res.append(a+b+c+d)
     return res
 
 ################################################################################
 
-db = './l_amat/HD.dat'
+def group_callsigns(remaining):
+    grouped_callsigns = {str(i): [] for i in range(10)}
+    for callsign in remaining:
+        for digit in range(10):
+            if str(digit) in callsign:
+                grouped_callsigns[str(digit)].append(callsign)
+                break
+    return grouped_callsigns
+
+def print_grouped_callsigns(grouped_callsigns):
+    max_column_width = max(len(callsigns) for callsigns in grouped_callsigns.values())
+    columns = len(grouped_callsigns)
+    for i in range(max_column_width):
+        for j in range(columns):
+            digit = str(j)
+            callsigns = grouped_callsigns[digit]
+            callsigns.sort()  # Sort call signs alphabetically
+            if i < len(callsigns):
+                print(f"{callsigns[i]:<8}", end="")
+            else:
+                print(" " * 8, end="")
+        print()
+
+db = './HD.dat'
 
 possibilities = one_by_two() + two_by_one()
 
@@ -65,7 +87,8 @@ with open(db) as csvfile:
 remaining = []
 for callsign in possibilities:
     if callsign not in in_use:
-        remaining.append( callsign )
+        remaining.append(callsign)
 
-print remaining
-print len(remaining)
+grouped_callsigns = group_callsigns(remaining)
+print_grouped_callsigns(grouped_callsigns)
+print("Total remaining call signs:", len(remaining))
